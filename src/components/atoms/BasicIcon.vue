b69028aa94a4c1320562f7e0736f4bbcb1870673
t.yoshida <bigriv1344@gmail.com>
2022-09-11 22:46:35 +0900

    add:プラグイン、ライブラリ追加 - plugins   - vue-cli-plugin-fontawesame - dependencies   - vuex   - vue-router   - sass-loader   - node-sass   - storybook   - storycap

---
+++ src/components/atoms/BasicIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <font-awesome-icon :icon="icon" />
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+  props: {
+    icon: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        return ["circle-user"].includes(value);
+      },
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCircleUser);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon ).mount("#app");
