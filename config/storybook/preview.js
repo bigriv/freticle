e9e8a50fe13b4bcc3a2693209e5eb8eb2c9f3d2d
t.yoshida <bigriv1344@gmail.com>
2022-09-19 11:40:12 +0900

    add: ライブラリ追加 - dependencies   - v-click-outside

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,23 @@
-import { app } from '@storybook/vue3';
+import { app } from "@storybook/vue3";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalendarDays,
+  faCircleUser,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
+import "v-calendar/dist/style.css";
+import { SetupCalendar, DatePicker } from "v-calendar";
+
+import vClickOutside from 'click-outside-vue3'
+
+library.add(faCalendarDays);
 library.add(faCircleUser);
+library.add(faMagnifyingGlass);
 
-app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(SetupCalendar, {})
+app.use(vClickOutside)
+
+app.component("font-awesome-icon", FontAwesomeIcon).component('DatePicker', DatePicker);
--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import App from "./App.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalendarDays,
+  faCircleUser,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
+import "v-calendar/dist/style.css";
+import { SetupCalendar, DatePicker } from "v-calendar";
+
+import vClickOutside from 'click-outside-vue3'
+
+library.add(faCalendarDays);
 library.add(faCircleUser);
+library.add(faMagnifyingGlass);
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon ).mount("#app");
+createApp(App)
+  .use(SetupCalendar, {})
+  .use(vClickOutside)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .component('DatePicker', DatePicker)
+  .mount("#app");
